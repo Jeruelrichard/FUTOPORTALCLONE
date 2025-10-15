1e0d4c9f8d4b2374c8963c3b4dc02141f40ecdf6
Jeruelrichard <okemdinach383@gmail.com>
2025-10-15 13:07:43 +0100

    modal successfully showing and desktop mode complete

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Storing Announcements as an Array of Objects
 const announcements = [{
     title: "IMPORTANT DISCLAIMER!!!",
     date: "Wed, 6th Aug 2025",
@@ -8,7 +9,7 @@ const announcements = [{
     Thanks
     Registrar
     
-    FUTO ICTC`
+    <b>FUTO ICTC<b>`
 },{
     title: "CANDIDATES DETAILS FOR O'LEVEL VERIFICATION 2025/26",
     date: "Fri, 3rd Oct 2025",
@@ -18,16 +19,17 @@ const announcements = [{
     
     Among others, each candidate is required to provide scratch card PIN and NUMBER to enable the verification of O'Level results. Candidates are required to provide the right information and should note that wrong details will delay or deny clearance to anyone.
     
-    FUTO ICTC`
+    <b>FUTO ICTC<b>`
 }];
 
 function openModal(index) {
-    const announcement = announcements[index];
-    document.getElementById('modalTitle').textContent = announcement.title;
-    document.getElementById('modalDate').textContent = announcement.date;
-    document.getElementById('modalBody').textContent = announcement.content;
+    const announcement = announcements[index]; //Picks the right announcement by its index
+    //Filling the modal's title, date and body with the announcement's data.
+    document.getElementById('modalTitle').innerHTML = announcement.title;
+    document.getElementById('modalDate').innerHTML = announcement.date;
+    document.getElementById('modalBody').innerHTML = announcement.content;
     document.getElementById('modal').classList.add('active');
-    document.body.style.overflow = 'hidden';
+    document.body.style.overflow = 'hidden'; //disables page scrolling while modal is active
 }
 
 function closeModal() {
@@ -35,6 +37,7 @@ function closeModal() {
     document.body.style.overflow = 'auto';
 }
 
+//The below ensures that clicking outside the whitebox closes the modal
 function closeModalOnBackdrop(event) {
     if (event.target.id === 'modal') {
         closeModal();
